ac0c58de014cb93cf13e2efb4bc0344857a7609d
tiago <tiagomateus.fonte13@ua.pt>
2022-03-26 15:28:00 +0000

    camera_test

--- camera_test/camera_test.py
+++ camera_test/camera_test.py
@@ -1,1 +1,14 @@
-SESGAWRG
+import cv2
+def main():
+    frameWidth = 640
+    frameHeight = 480
+    cap = cv2.VideoCapture(0)
+    cap.set(3, frameWidth)
+    cap.set(4,frameHeight)
+    while True:
+        success, img = cap.read()
+        cv2.imshow('Result', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+if __name__ == "__main__":
+    main()
