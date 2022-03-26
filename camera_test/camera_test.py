a347f723132913ffb4fcbfc9452747e74a7b8969
tiago <tiagomateus.fonte13@ua.pt>
2022-03-26 16:00:31 +0000

    faltava a shebang line

--- camera_test/camera_test.py
+++ camera_test/camera_test.py
@@ -5,7 +5,7 @@ def main():
     frameHeight = 480
     cap = cv2.VideoCapture(0)
     cap.set(3, frameWidth)
-    cap.set(4,frameHeight)
+    cap.set(4, frameHeight)
     while True:
         success, img = cap.read()
         cv2.imshow('Result', img)
