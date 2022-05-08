1fa600b8db57e779d7461b816defb26e1bfe737b
tiago <tiagomateus.fonte13@ua.pt>
2022-05-08 22:27:57 +0100

    added code for capturing sensor data

--- camera_test/processamento.py
+++ camera_test/processamento.py
@@ -10,38 +10,41 @@ import numpy as np
 import json
 import datetime
 import colorsys
+
+
 def main():
     f = open('limits.json')
     data = json.load(f)
-    mins = np.array([data['limits']['B']['min'], data['limits']['G']['min'], data['limits']['R']['min']])       # Gets minimum RGB color values from data variable.
+    mins = np.array([data['limits']['B']['min'], data['limits']['G']['min'], data['limits']['R']['min']])  # Gets minimum RGB color values from data variable.
     maxs = np.array([data['limits']['B']['max'], data['limits']['G']['max'], data['limits']['R']['max']])
-    #image_processed = cv2.inRange(image, mins, maxs)
+    # image_processed = cv2.inRange(image, mins, maxs)
     capture = cv2.VideoCapture(0)
     window_name = 'Original'
     window_name2 = 'Processed'
-    cv2.namedWindow(window_name,cv2.WINDOW_AUTOSIZE)
-    cv2.namedWindow(window_name2,cv2.WINDOW_AUTOSIZE)
+    cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
+    cv2.namedWindow(window_name2, cv2.WINDOW_AUTOSIZE)
     while True:
         _, image = capture.read()  # get an image from the camera
         image_processed = cv2.inRange(image, mins, maxs)
         
         m = cv2.moments(image_processed)
         print(m)
-        #x = m['m10']/m['m00']
-        #y = m['m01']/m['m00']
-        #print('x=',x, 'y=',y)
-        if m['m00'] == 0 :
+        # x = m['m10']/m['m00']
+        # y = m['m01']/m['m00']
+        # print('x=',x, 'y=',y)
+        if m['m00'] == 0:
             pass
         else:
 
             x = m['m10']/m['m00']
             y = m['m01']/m['m00']
 
-        # add code to show acquired image
-        #cv2.circle(image_processed, (int(x),int(y)), 5, 255, 5)
-            cv2.circle(image_processed,(int(x), int(y)), 10, (0,0,255), -1)
+            # add code to show acquired image
+            # cv2.circle(image_processed, (int(x),int(y)), 5, 255, 5)
+
+            cv2.circle(image_processed, (int(x), int(y)), 10, (0, 0, 255), -1)
             cv2.imshow(window_name2, image_processed)
-            cv2.circle(image,(int(x), int(y)), 10, (0,0,255), -1)
+            cv2.circle(image, (int(x), int(y)), 10, (0, 0, 255), -1)
             cv2.imshow(window_name, image)
             # add code to wait for a key press
             cv2.waitKey(20)
