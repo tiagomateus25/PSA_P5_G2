1739e37f92eb5e1eb67545e9b974e9d59cc40603
tiago <tiagomateus.fonte13@ua.pt>
2022-06-18 23:58:04 +0100

    testing xbox controller

--- pydrone/PID.py
+++ pydrone/PID.py
@@ -8,11 +8,11 @@ import readchar
 from colorama import Fore, Back, Style
 import json
 import socket
-from ESCs_calibration import calibrate
+
+
 os.system("sudo pigpiod")   # Launching GPIO library
 time.sleep(1)
 pi = pigpio.pi()
-
 mpu = mpu6050(0x68)     # check if it's the right pin
 
 
@@ -27,7 +27,45 @@ throttle = 1300
 
 
 def calibration():  # This is the auto calibration procedure of a normal ESC
-    calibrate()
+
+    print('You have chosen ESCs calibration. Follow the instructions to proceed.')
+    max_value = 2400  # change this if your ESC's max value is different or leave it
+    min_value = 700  # change this if your ESC's min value is different or leave it
+    pi.set_servo_pulsewidth(motor27, 0)
+    pi.set_servo_pulsewidth(motor19, 0)
+    pi.set_servo_pulsewidth(motor20, 0)
+    pi.set_servo_pulsewidth(motor24, 0)
+    print("Disconnect the battery and press Enter")
+    inp_a = input()
+    if inp_a == '':
+        pi.set_servo_pulsewidth(motor27, max_value)
+        pi.set_servo_pulsewidth(motor19, max_value)
+        pi.set_servo_pulsewidth(motor20, max_value)
+        pi.set_servo_pulsewidth(motor24, max_value)
+        print("Connect the battery. Maximum speed is being acquired, wait for the next instruction.")
+        time.sleep(20)
+        print('Press Enter to continue.')
+        inp_b = input()
+        if inp_b == '':
+            pi.set_servo_pulsewidth(motor27, min_value)
+            pi.set_servo_pulsewidth(motor19, min_value)
+            pi.set_servo_pulsewidth(motor20, min_value)
+            pi.set_servo_pulsewidth(motor24, min_value)
+            print('Minimum speed is being acquired.')
+            time.sleep(7)
+            time.sleep(5)
+            pi.set_servo_pulsewidth(motor27, 0)
+            pi.set_servo_pulsewidth(motor19, 0)
+            pi.set_servo_pulsewidth(motor20, 0)
+            pi.set_servo_pulsewidth(motor24, 0)
+            time.sleep(2)
+            print('Arming ESCs.')
+            pi.set_servo_pulsewidth(motor27, min_value)
+            pi.set_servo_pulsewidth(motor19, min_value)
+            pi.set_servo_pulsewidth(motor20, min_value)
+            pi.set_servo_pulsewidth(motor24, min_value)
+            time.sleep(1)
+            print('Calibration complete.')
 
 
 def controller():
@@ -329,7 +367,7 @@ def instructions():
     inp = input()
     if inp == "calibrate":
         print('\n')
-        calibrate()
+        calibration()
     if inp == "key":
         print('\n')
         key_control()
@@ -363,7 +401,7 @@ def main():
     inp = input()
     if inp == "calibrate":
         print('\n')
-        calibrate()
+        calibration()
     if inp == "key":
         print('\n')
         key_control()
