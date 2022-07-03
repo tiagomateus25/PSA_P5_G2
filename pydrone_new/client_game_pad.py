c1cfd9cece8da913db9214914abdaceed9297b28
tiago <tiagomateus.fonte13@ua.pt>
2022-07-03 23:23:25 +0100

    corrected more code

--- pydrone_new/client_game_pad.py
+++ pydrone_new/client_game_pad.py
@@ -9,7 +9,7 @@ import time
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening----------------------------------------------------------
-server_address = ('192.168.143.11', 10000)
+server_address = ('192.168.113.11', 10000)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
 
